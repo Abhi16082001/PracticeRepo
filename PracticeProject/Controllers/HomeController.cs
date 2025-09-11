c81d427dc15e5ecdffc2a939ce09a9da7da6b076
Abhishek Rajput <abhishekrajput16082001@gmail.com>
2025-09-11 13:34:43 +0530

    Fixed credentials

---
+++ PracticeProject/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using PracticeProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticeProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        Employee obj = new Employee();
+        public ActionResult Index()
+        {          
+            TempData["Hobbies"] = obj.hobbylist;
+            TempData["countries"]=obj.countrylist;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(Employee emp, FormCollection col)
+        {            
+            string hoby = string.Empty;
+            foreach(var item in obj.hobbylist)
+            {
+                if (col[item].ToString().Contains("true"))
+                {
+                    hoby += item + ", ";
+                }
+            }
+            obj.Hobbies = hoby;
+            obj.country = emp.country;
+            TempData["Hobbies"] = obj.hobbylist;
+            TempData["countries"] = obj.countrylist;
+            return View("Index",obj);
+        }
+    }
+}
---
+++ PracticeProject/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PracticeProject.Models
+{
+    public class Employee
+    {
+
+        public Employee()
+        {
+            countrylist = new List<string>();
+            hobbylist = new List<string>();
+            countrylist.Add("India");
+            countrylist.Add("USA");
+            countrylist.Add("China");
+            countrylist.Add("UK");
+            countrylist.Add("Russia");
+            hobbylist.Add("Singing");
+            hobbylist.Add("Dancing");
+            hobbylist.Add("Painting");
+            hobbylist.Add("Writing");
+        }
+
+        public string Hobbies { get; set; }
+        public string country { get; set; }
+        public List<string> countrylist { get; set; }      
+        public List<string> hobbylist { get; set; }
+    }
+}
